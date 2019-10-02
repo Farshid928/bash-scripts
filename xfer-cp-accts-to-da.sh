14184b0807b03aa1edbb419254ce74f4e21bdaa4
FaЯshid <farshid928@gmail.com>
2019-10-02 20:18:05 +0330

    Update xfer-cp-accts-to-da.sh

--- xfer-cp-accts-to-da.sh
+++ xfer-cp-accts-to-da.sh
@@ -7,8 +7,6 @@
 
 #Note that the connection between servers must be established via the ssh key.
 
-#Important note that none of cpanel accounts should be suspended! If so, the script will not work correctly.
-
 DEST="192.168.1.1" #Your DirectAdmin Server IP
 PORT="22" #Your Directadmin Server ssh Port
 cd /var/cpanel/users
@@ -21,6 +19,7 @@ then
         #backup cpanel account (public_html excludes) and transfer to directadmin server.
         /scripts/pkgacct $USER --skippublichtml
         rsync -av -e "ssh -p $PORT" /home/cpmove-$USER.tar.gz root@$DEST:/backup/
+	DOMAIN=$(cat /var/cpanel/users/$USER | grep -w DNS | awk -v FS='=' '{print $2}')
 
 	#Restore backup in directadmin server.
 	ssh -p $PORT root@$DEST "
@@ -30,18 +29,30 @@ then
 		echo "backup is restoring in DirectAdmin!"
 		sleep 2
 		while \$(grep -Fq "action=restore" /usr/local/directadmin/data/task.queue 2>/dev/null); do printf "%s""$i" .; sleep 2; done;
-		while \$(test ! -d /home/$USER/public_html); do printf "%s""$i" .; sleep 2; done;
+		while \$(test ! -d /home/$USER/domains/$DOMAIN/public_html); do printf "%s""$i" .; sleep 2; done;
 		printf '\n'
 		echo "backup restoration completed in DirectAdmin!"
 		sleep 2
 	"
 	
 	#transfer public_html files and dirs to destinaton server	
-	rsync -av -e "ssh -p $PORT" /home/$USER/public_html/ root@$DEST:/home/$USER/public_html/
+	rsync -av -e "ssh -p $PORT" /home/$USER/public_html/ root@$DEST:/home/$USER/domains/$DOMAIN/public_html/
 	ssh -p $PORT root@$DEST "
-		chown -R $USER.$USER /home/$USER/public_html/
-		find /home/$USER/public_html/ -type d -exec chmod 755 {} \;
+		chown -R $USER.$USER /home/$USER/domains/$DOMAIN/public_html/
+		find /home/$USER/domains/$DOMAIN/public_html/ -type d -exec chmod 755 {} \;
 	"
+	
+	#transfer addon domians files and foldres to the correct directory
+	awk '/addon_domains/,/main_domain/' /var/cpanel/userdata/$USER/main | awk -v FS=':' '{print $1}' | grep -v _domain | cut -c 3- > tmp.txt
+	while read DOMAIN  ; do
+	echo "move $DOMAIN files"
+	ssh -p $PORT root@$DEST "
+		rsync -av --remove-source-files /home/$USER/public_html/$DOMAIN/ /home/$USER/domains/$DOMAIN/public_html/
+		rm -rf /home/$USER/public_html/$DOMAIN/
+	" < /dev/null
+	sleep 2
+	done < tmp.txt
+	rm tmp.txt
 
 	#remove backup files in order to free space
 	ssh -p $PORT root@$DEST "rm /backup/cpmove-$USER.tar.gz"
